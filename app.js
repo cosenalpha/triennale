1aee36563267b8445d84e93a82cbd467f59ec2ce
francescobonetti <francesco2.bonetti@mail.polimi.it>
2023-03-18 19:53:20 +0000

    menu mobile fatto + immagini home sostituite

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 gsap.registerPlugin(ScrollTrigger);
 
 const hamburger = document.querySelector(".hamburger");
+const cross = document.querySelector("#cross");
 const navMenu = document.querySelector(".text-container");
+const body = document.querySelector("body");
 
 hamburger.addEventListener("click", mobileMenu);
+cross.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
+  body.classList.toggle("stop");
 }
 
 
