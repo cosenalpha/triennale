35bc46613e550aad973ecdf4c93601ccd18e5460
francescobonetti <francesco2.bonetti@mail.polimi.it>
2023-03-18 17:55:55 +0000

    Scroll home sistemato con scritte arrotondate e vuote con mix-blend-mode difference

--- app.js
+++ app.js
@@ -14,53 +14,53 @@ function mobileMenu() {
 let progetti = Array.from(document.querySelectorAll(".nome-progetto")) 
 let immagini = Array.from(document.querySelectorAll("#container-immagine img"))
 
-for(let i = 0; i < progetti.length; i++) {
+/* for(let i = 0; i < progetti.length; i++) {
 
     progetti[i].addEventListener("mouseenter", attivaImmagine)
 
     function attivaImmagine() {immagini[i].style.opacity = 1}
 
-    
     progetti[i].addEventListener("mouseleave", disattivaImmagine)
 
     function disattivaImmagine() {immagini[i].style.opacity = 0}
 
 }
-
-
-//let mobile = window.matchMedia("(max-width: 600px)")
+ */
 
 let mobile = window.matchMedia("(max-width: 600px)")
 
+
 function update() {
+
+  let enter = 20 / 100 * window.innerHeight
+  let escher = 80 / 100 * window.innerHeight
+
+  if(mobile.matches) {
+    enter = 30 / 100 * window.innerHeight
+    escher = 70 / 100 * window.innerHeight
+  }
   
-  for(let i = 0; i < progetti.length; i++) {
+  for(let i = 0; i < progetti.length; i++) {  
     
-    let y = progetti[i].getBoundingClientRect().y 
-    let enter = 40 / 100 * window.innerHeight
-    let escher = 59.3 / 100 * window.innerHeight
+    let y = progetti[i].getBoundingClientRect().y + progetti[i].getBoundingClientRect().height / 2 
 
     if (y > enter && y < escher ) {
-      progetti[i].querySelector(".border").style.opacity = 1
-      progetti[i].querySelector(".fill").style.opacity = 0
+      /* progetti[i].querySelector(".border").style.opacity = 0 */
+      progetti[i].querySelector(".fill").style.opacity = 1
       immagini[i].style.opacity = 1
     }
     else {
-      progetti[i].querySelector(".fill").style.opacity = 1
-      progetti[i].querySelector(".border").style.opacity = 0
+      /* progetti[i].querySelector(".fill").style.opacity = 0 */
+      progetti[i].querySelector(".fill").style.opacity = 0.2
       immagini[i].style.opacity = 0
     }
   }
 
-
 }
 
+setInterval(update, 50)  
 
-if(mobile.matches) {
+ 
 
-  setInterval(update, 6)  
 
-  console.log("mobile device")
-
-}
 
