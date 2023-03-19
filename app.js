cadad0e1ab45cb4e2feb77745fc94e31de542383
claravaggio <clara.dibella@mail.polimi.it>
2023-03-19 22:40:35 +0100

    fixing flogisto

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+const hamburger = document.querySelector(".hamburger");
+const cross = document.querySelector("#cross");
+const navMenu = document.querySelector(".text-container");
+const body = document.querySelector("body");
+
+hamburger.addEventListener("click", mobileMenu);
+cross.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+  body.classList.toggle("stop");
+}
+
 const itversion = document.getElementById('itversion');
 		const enversion = document.getElementById('enversion');
 		const italianText = document.querySelectorAll('.it');
@@ -33,19 +47,7 @@ const itversion = document.getElementById('itversion');
 
 
 
-const hamburger = document.querySelector(".hamburger");
-const cross = document.querySelector("#cross");
-const navMenu = document.querySelector(".text-container");
-const body = document.querySelector("body");
 
-hamburger.addEventListener("click", mobileMenu);
-cross.addEventListener("click", mobileMenu);
-
-function mobileMenu() {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
-  body.classList.toggle("stop");
-}
 
 
 
