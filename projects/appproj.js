aabd3315d6816d4eb6de17eaec2e412b5a8cd773
agosanna <agostino.sanna@mail.polimi.it>
2023-03-19 15:37:39 +0100

    sistemata pagina progetto (desktop e mobile)
    manca ancora titlo e payoff

--- projects/appproj.js
+++ projects/appproj.js
@@ -1,6 +1,3 @@
-// gsap.registerPlugin(ScrollTrigger);
-
-
 //hamburger menu mobile
 
 const hamburger = document.querySelector(".hamburger");
@@ -11,65 +8,8 @@ hamburger.addEventListener("click", mobileMenu);
 function mobileMenu() {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
+  cross.style.display("block");
 }
-
-// //scroll pin
-
-// setTimeout(attivaGsap, 3000)
-
-// function attivaGsap() {
-
-//     let mm = gsap.matchMedia();
-
-// mm.add("(min-width: 600px)", () => {
-//     gsap.to("#text1", {
-//         duration: 8,
-//         scrollTrigger: {
-//             trigger: "#scenario",
-//             start: "top 15%",
-//             end: "bottom 40%",
-//             scrub: 4,
-//             toggleActions: "restart none none none",
-//             pin: "#text1",
-//             markers: false,
-//             pinSpacing: false
-//         }
-//   });
-// });
-
-// mm.add("(min-width: 600px)", () => {
-//     gsap.to("#text2", {
-//         duration: 8,
-//         scrollTrigger: {
-//             trigger: "#concept",
-//             start: "top 15%",
-//             end: "bottom 60%",
-//             scrub: 4,
-//             toggleActions: "restart none none none",
-//             pin: "#text2",
-//             markers: false,
-//             pinSpacing: false
-//         }
-//   });
-// });
-
-// mm.add("(min-width: 600px)", () => {
-//     gsap.to("#text3", {
-//         duration: 8,
-//         scrollTrigger: {
-//             trigger: "#interazione",
-//             start: "top 15%",
-//             end: "bottom 65%",
-//             scrub: 4,
-//             toggleActions: "restart none none none",
-//             pin: "#text3",
-//             markers: true,
-//             pinSpacing: false
-//         }
-//   });
-// });
-
-// }
 
 
 
